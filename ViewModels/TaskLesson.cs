643b93d4ff7fe1c19ad2ba86d92158bcc5927356
taggeddev <denis.mikov.06@gmail.com>
2021-12-30 11:51:11 +0300

    feat(viewmodels): new viewmodels to connect tasklesson and tasktheme

---
+++ ViewModels/TaskLesson.cs
@@ -0,0 +1,10 @@
+﻿using LingvaApp.Models;
+
+namespace LingvaApp.ViewModels
+{
+    public class TaskLesson
+    {
+        public Task Task { get; set; }
+        public Lesson Lesson { get; set; }
+    }
+}
---
+++ ViewModels/TaskTheme.cs
@@ -0,0 +1,10 @@
+﻿using LingvaApp.Models;
+
+namespace LingvaApp.ViewModels
+{
+    public class TaskTheme
+    {
+        public Task Task { get; set; }
+        public Theme Theme { get; set; }
+    }
+}
