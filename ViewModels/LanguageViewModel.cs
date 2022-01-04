95d9c0e4348cf90487102eaf548d4ff825279f0c
taggeddev <denis.mikov.06@gmail.com>
2022-01-04 13:13:13 +0300

    add(viewmodel, view): New files to generate lesson

---
+++ ViewModels/LanguageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using LingvaApp.Models;
+using System.Collections.Generic;
+
+namespace LingvaApp.ViewModels
+{
+    public class LanguageViewModel
+    {
+        public List<Theme> Themes { get; set; }
+        public List<Field> Fields { get; set; }
+        public List<Task> Tasks { get; set; }
+    }
+}
