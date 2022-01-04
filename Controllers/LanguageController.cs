52769f038d880730f242f6f2978f39c2176310db
taggeddev <denis.mikov.06@gmail.com>
2022-01-04 13:12:45 +0300

    feat(controller): Changes in controller to load themes from database

--- Controllers/LanguageController.cs
+++ Controllers/LanguageController.cs
@@ -1,26 +1,62 @@
-﻿using LingvaApp.Models;
+﻿using LingvaApp.Data;
+using LingvaApp.Models;
+using LingvaApp.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace LingvaApp.Controllers
 {
     public class LanguageController : Controller
     {
+        private ApplicationDbContext _dbContext;
+
+        public LanguageController(ApplicationDbContext context)
+        {
+            _dbContext = context;
+        }
+
+
         public IActionResult English()
         {
-            Task lesson = new Task();
-            return View(lesson);
+            LanguageViewModel viewmodel = new LanguageViewModel()
+            {
+                Themes = _dbContext.Themes.Where(x => x.LanguageParent == "English").ToList()
+            };
+            return View(viewmodel);
         }
 
         public IActionResult Russian()
         {
-            Task lesson = new Task();
-            return View(lesson);
+            LanguageViewModel viewmodel = new LanguageViewModel()
+            {
+                Themes = _dbContext.Themes.Where(x => x.LanguageParent == "Русский").ToList()
+            };
+            return View(viewmodel);
         }
 
-        public IActionResult Deutch()
+        public IActionResult Deutsch()
         {
-            Task lesson = new Task();
-            return View(lesson);
+            LanguageViewModel viewmodel = new LanguageViewModel()
+            {
+                Themes = _dbContext.Themes.Where(x => x.LanguageParent == "Deutsch").ToList()
+            };
+            return View(viewmodel);
+        }
+    
+        public IActionResult Lesson(int theme, int taskid)
+        {
+            // If task id == 0, it should load first element. Else - load fields by taskId
+            ViewBag.LoadId = taskid;
+            ThemeViewModel model = new ThemeViewModel(_dbContext, theme);
+            if (taskid == 0)
+            {
+                ViewBag.Fields = model.Fields[0];
+            }
+            else
+            {
+                ViewBag.Fields = model.Fields.Where(x => x[0].TaskParentID == taskid);
+            }
+            return View(model);
         }
     }
 }
