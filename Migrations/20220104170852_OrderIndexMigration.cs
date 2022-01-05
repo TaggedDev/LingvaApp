87aa8575f40df66ff913e68da7ab7ead3d016b8d
taggeddev <denis.mikov.06@gmail.com>
2022-01-05 11:28:36 +0300

    feat(database): added migration for changes in models

---
+++ Migrations/20220104170852_OrderIndexMigration.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LingvaApp.Migrations
+{
+    public partial class OrderIndexMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LanguageLevelColumnPosition",
+                table: "Themes");
+
+            migrationBuilder.DropColumn(
+                name: "ThemeTaskPosition",
+                table: "Tasks");
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderIndex",
+                table: "Themes",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderIndex",
+                table: "Tasks",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderIndex",
+                table: "Field",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderIndex",
+                table: "Themes");
+
+            migrationBuilder.DropColumn(
+                name: "OrderIndex",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "OrderIndex",
+                table: "Field");
+
+            migrationBuilder.AddColumn<int>(
+                name: "LanguageLevelColumnPosition",
+                table: "Themes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ThemeTaskPosition",
+                table: "Tasks",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
