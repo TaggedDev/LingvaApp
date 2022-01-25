b30403702bdbb9a0c86023be487cb1c7f4547f53
taggeddev <denis.mikov.06@gmail.com>
2022-01-25 14:04:53 +0300

    feat(models): Added pending and published articles models

---
+++ Models/PendingArticle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LingvaApp.Models
+{
+    public class PendingArticle
+    {
+        [Key]
+        public int ArticleID { get; set; }
+        public string AuthorID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+        public string Language { get; set; }
+        public string Level { get; set; }
+        public string Tags { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
