d2c10ed1ab6d60b1ad56d35b2ff5a0ef29d83a84
taggeddev <denis.mikov.06@gmail.com>
2022-01-28 23:54:52 +0300

    refactor(models): Added thumbnail url and image fields

--- Models/PublishedArticle.cs
+++ Models/PublishedArticle.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LingvaApp.Models
 {
@@ -8,8 +10,13 @@ namespace LingvaApp.Models
         [Key]
         public int ArticleID { get; set; }
         public string AuthorID { get; set; }
+        public string AuthorAvatarURL { get; set; }
+        public string AuthorUsername { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string ThumbnailURL { get; set; }
+        [NotMapped]
+        public IFormFile ThumbnailPicture { get; set; }
         public string Content { get; set; }
         public int Rating { get; set; }
         public string Language { get; set; }
