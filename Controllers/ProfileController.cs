f10b58f181a5bdc4edccd5fced8def71bb6907e4
taggeddev <denis.mikov.06@gmail.com>
2022-01-29 13:57:32 +0300

    feat(mvc): Added profile page

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,26 @@
+﻿using LingvaApp.Data;
+using LingvaApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LingvaApp.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _dbContext;
+
+        public ProfileController(UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _dbContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
