96ef4c1b6e7820f8bff2f110e77ddf4d769b0593
taggeddev <denis.mikov.06@gmail.com>
2022-02-12 19:39:55 +0300

    feat(db): added default roles and default admin account

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -13,9 +13,6 @@ namespace LingvaApp.Data
         public DbSet<PendingArticle> PendingArticles { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { base.OnModelCreating(modelBuilder); }
     }
 }
---
+++ Models/IdentityInitializer.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace LingvaApp.Models
+{
+    public class IdentityInitializer
+    {
+        public static async void InitializeAsync(IServiceProvider provider)
+        {
+            RoleManager<IdentityRole> roleManager = provider.GetRequiredService<RoleManager<IdentityRole>>();
+            UserManager<ApplicationUser> userManager = provider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            var adminUser = await userManager.FindByNameAsync("sarancha");
+
+            if (await roleManager.FindByNameAsync("Admin") == null)
+                await roleManager.CreateAsync(new IdentityRole("Admin"));
+            
+            if (await roleManager.FindByNameAsync("Moderator") == null)
+                await roleManager.CreateAsync(new IdentityRole("Moderator"));
+
+            if (await roleManager.FindByNameAsync("User") == null)
+                await roleManager.CreateAsync(new IdentityRole("User"));
+
+            if (adminUser == null)
+                await userManager.CreateAsync(adminUser, "123qwe");
+            await userManager.AddToRoleAsync(adminUser, "Admin");
+        }
+    }
+}
