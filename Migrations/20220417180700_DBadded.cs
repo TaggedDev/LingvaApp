ed6c045555d6bbd6830d94d584025b41941db520
GalteXx <65533265+GalteXx@users.noreply.github.com>
2022-04-17 22:52:30 +0300

    Style for Error message

---
+++ Migrations/20220417180700_DBadded.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LingvaApp.Migrations
+{
+    public partial class DBadded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
